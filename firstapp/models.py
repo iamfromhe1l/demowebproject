8bd436380680f2e367b23067b0b7e4d86e7178a7
iamfromhe1l <sgadgsfad@gmail.com>
2022-06-14 19:22:50 +0300

    adding new items on page with sql and djform

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=10)
+    str = models.IntegerField()
+    value = models.IntegerField()
